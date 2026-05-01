0fedfc2f36094b99387887f31b9cb4588cc177d7
Myangaa97 <myangaa97@gmail.com>
2026-05-02 00:22:14 +0800

    home

--- month02/week05/day05/ATMSimulator.java
+++ month02/week05/day05/ATMSimulator.java
@@ -21,12 +21,12 @@ public class ATMSimulator {
                 logged = 1;
 
             }
-            
+
             else {
-                attemps --;
-            
-            if (attemps > 0) {
-                    
+                attemps--;
+
+                if (attemps > 0) {
+
                     System.out.println("PIN буруу. " + attemps + " оролдлого үлдлээ");
                 } else {
                     System.out.println("Оролдлого дууслаа. Карт түгжигдлээ.");
@@ -35,52 +35,54 @@ public class ATMSimulator {
         }
 
         while (logged == 1 && select != 4) {
-        System.out.println("╔═══════════════════╗");
-        System.out.println("║   Үйлдэл сонгоно  ║");
-        System.out.println("║ 1. Үлдэгдэл       ║");
-        System.out.println("║ 2. Мөнгө авах     ║");
-        System.out.println("║ 3. Мөнгө хийх     ║");
-        System.out.println("║ 4. Гарах          ║");
-        System.out.println("╚═══════════════════╝");
-        System.out.println( );
+            System.out.println("╔═══════════════════╗");
+            System.out.println("║   Үйлдэл сонгоно  ║");
+            System.out.println("║ 1. Үлдэгдэл       ║");
+            System.out.println("║ 2. Мөнгө авах     ║");
+            System.out.println("║ 3. Мөнгө хийх     ║");
+            System.out.println("║ 4. Гарах          ║");
+            System.out.println("╚═══════════════════╝");
+            System.out.println();
 
-        select = sc.nextInt();
+            select = sc.nextInt();
             switch (select) {
-                case 1: 
-                    System.out.printf(balance + "₮");
-                    break;  
+                case 1:
+                    System.out.println(balance + "₮");
+                    break;
                 case 2:
                     System.out.println("Авах дүн оруулна уу : ");
                     int deposit = sc.nextInt();
-                        if (deposit < 500000) {
-                            balance = balance - deposit;
-                            System.out.println(deposit + "₮ олгов.");
-                        } else {
-                            System.out.println("Хязгаар хэтэрлээ (500,000₮)");
-                        } break;
+                    if (deposit < 500000) {
+                        balance = balance - deposit;
+                        System.out.println(deposit + "₮ олгов.");
+                    } else {
+                        System.out.println("Хязгаар хэтэрлээ (500,000₮)");
+                    }
+                    break;
 
                 case 3:
                     System.out.println("Хийх дүн оруулна уу : ");
                     int withdraw = sc.nextInt();
-                        if (withdraw > 0) {
-                            balance = balance + withdraw;
-                        } else {
-                            System.out.println("Буруу дүн");
-                        } break;
+                    if (withdraw > 0) {
+                        balance = balance + withdraw;
+                    } else {
+                        System.out.println("Буруу дүн");
+                    }
+                    break;
 
                 case 4:
-                    System.out.println("Баярлалаа! Аюулгүй яваарай."); break;
-                            
+                    break;
+
             }
 
             if (select != 4) {
-                System.out.printf("Дахин үйлдэл хийх үү? (1-Тийм / 4-Гарах): ");
-                select = sc.nextInt(); 
+                System.out.println("Дахин үйлдэл хийх үү? (1-Тийм / 4-Гарах): ");
+                select = sc.nextInt();
             }
             System.out.println("Баярлалаа! Аюулгүй яваарай.");
         }
-        
+
         sc.close();
     }
-       
+
 }
