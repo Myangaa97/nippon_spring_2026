6648a9482194b60a27198551cbe6536a7e10740c
myangaa <myangaa97@gmail.com>
2026-06-10 18:26:23 +0800

    jkhfd

--- iCodeCourse/month03/week11/day02/backend/src/test/java/mn/icode/TestSerializable.java
+++ iCodeCourse/month03/week11/day02/backend/src/test/java/mn/icode/TestSerializable.java
@@ -1,5 +1,35 @@
 package mn.icode;
 
 public class TestSerializable {
+	@Test
+    void testUserSerialize() {
+        User user = new User("Bold", "bold@mail.com");
+        assertEquals("User{name=Bold, email=bold@mail.com}", user.serialize()
+        );
+    }
 	
+	@Test
+    void configSerializeTest() {
+        Config config = new Config("color", "black");
+        assertEquals("Config{key=color, value=black}",
+            config.serialize()
+        );
+    }
+
+    @Test
+    void defaultPrintDoesNotThrow() {
+        User user = new User("Bob", "bob@mail.com");
+
+        assertDoesNotThrow(() -> {user.print();
+        });
+    }
+
+    @Test
+    void interfacePolymorphismTest() {
+        Serializable s1 = new User("Tom", "tom@mail.com");
+        Serializable s2 = new Config("lang", "en");
+
+        assertTrue(s1.serialize().contains("User"));
+        assertTrue(s2.serialize().contains("Config"));
+    }
 }
