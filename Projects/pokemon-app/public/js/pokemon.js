5120143e371a794e3967d991f4bd08e5a4a9209d
myangaa <myangaa97@gmail.com>
2026-08-11 17:36:50 +0800

    w18-day3

--- Projects/pokemon-app/public/js/pokemon.js
+++ Projects/pokemon-app/public/js/pokemon.js
@@ -3,6 +3,10 @@ const PAGE_SIZE = 20; // neg udaagiin  duudah pokemon too
 let currentPage = 0;
 let isLoading = false;
 const POKEMON_CONTAINER = document.querySelector('#pokemon-grid');
+const typeSelect = document.getElementById('type-select');
+const modal = document.getElementById('pokemon-modal');
+const modalContent = document.getElementById('modal-content');
+const closeModalBtn = document.getElementById('close-modal');
 
 
 async function loadPokemons() {
@@ -18,7 +22,7 @@ async function loadPokemons() {
         // 1. get all list of pokemons
         const response = await fetch(listUrl);
         const data = await response.json();
-        console.log(data);
+        console.log('Full data', data);
 
         // 2. call every detail of pokemons
         const detailPromise = data.results.map(async (item) => {
@@ -26,16 +30,15 @@ async function loadPokemons() {
             const detailResponse = await fetch(item.url);
             return await detailResponse.json();
         });
-        console.log(detailPromise);
+        console.log('1 promise', detailPromise);
 
         // 3. bvh promise hvselt negtgeh
         const pokemonDetails = await Promise.all(detailPromise);
-        console.log(pokemonDetails);
+        console.log('all promise', pokemonDetails);
 
         // 4. Render pokemons to the pokemon containers
         pokemonDetails.forEach((pokemon) => {
             console.log(pokemon);
-
             renderCard(pokemon);
         });
 
@@ -72,6 +75,57 @@ function renderCard(pokemon) {
     POKEMON_CONTAINER.appendChild(card);
 }
 
+// Modal fuction
+async function openPokemonModal(id) {
+    modalContent.innerHTML = "<p>Loading .....</p>"
+    modal.showModal();
+
+    try {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const pokemon = await res.json();
+        console.log('modal pokemon: ', pokemon);
+
+        const artWork = pokemon.sprites.other['official-artwork'].front_default || pokemon.sprites.front_default;
+        modalContent.classList.add(pokemon.types[0].type.name);
+        modalContent.innerHTML = `
+
+            <div class="modal-head">
+                <div class="modal-img">
+                    <img src="${artWork}" alt=${pokemon.name} />
+                </div>
+                <div class="modal-h-text">
+                    <h4>#${String(pokemon.id).padStart(3, '0')}</h4>
+                    <h3>${pokemon.name}</h3>
+                </div>
+            </div>
+
+            <div class="modal-main">
+                <p>${pokemon.height}</p>
+                <p>${pokemon.weight}</p>
+            </div>
+            
+        `;
+    } catch (error) {
+        modalContent.innerHTML = "<p>Error...</p>";
+    }
+}
+
+// Setup Event
+POKEMON_CONTAINER.addEventListener('click', (event)=> {
+    
+    const clickedCard = event.target.closest('.card');
+    console.log(clickedCard.dataset.id);
+    if(clickedCard) {
+        const pokenmonId = clickedCard.dataset.id;
+        openPokemonModal(pokenmonId);
+    }
+
+    closeModalBtn.addEventListener('click', () => {
+        
+    });
+});
+
+
 const loader = document.querySelector("#loading-trigger");
 if (loader) {
     const observer = new IntersectionObserver((entries) => {
@@ -83,5 +137,3 @@ if (loader) {
 } else {
     loadPokemons();
 }
-
-
